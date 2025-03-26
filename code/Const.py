f2832064c7d573bc60b1d781330d672c097ef940
mbrgmn <mayarabergamini@outlook.com>
2025-03-26 18:55:46 -0300

    Creating asset folder Inserting all menu assets Creating the menu: backgroung, sound and fonts Creating the const file

---
+++ code/Const.py
@@ -0,0 +1,19 @@
+# C
+COLOR_YELLOW = (240, 214, 0)
+COLOR_WHITE = (255, 255, 255)
+COLOR_BROWN = (69, 39, 25)
+COLOR_BLUE = (28, 32, 66)
+COLOR_RED = (237, 9, 45)
+COLOR_PURPLE = (122, 0, 222)
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
+
