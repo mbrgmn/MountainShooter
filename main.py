25803defba4296d8a9b8d3bc749867122956b8e9
mbrgmn <107090048+mbrgmn@users.noreply.github.com>
2025-03-25 21:52:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ print('Loop Start')
 while True:
     # check for all events
     for event in pygame.event.get():
+        print('Quitting...')
         if event.type == pygame.QUIT:
             pygame.quit() # close window
             quit() # end pygame
