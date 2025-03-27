cebdded35dfb33a31533c93571c4495a123e72a6
mbrgmn <mayarabergamini@outlook.com>
2025-03-27 15:51:39 -0300

    Changing music file 'Level1.wav' for 'Level1.mp3' Changes on Level.py, Entity.py and Menu.py: convert_alpha()
    Level.py additions: printed text section, timeout and music background

--- code/Menu.py
+++ code/Menu.py
@@ -10,7 +10,7 @@ from code.Const import WIN_WIDTH, COLOR_YELLOW, MENU_OPTION, COLOR_WHITE, COLOR_
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./asset/Menubg.png')
+        self.surf = pygame.image.load('./asset/Menubg.png').convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self):
